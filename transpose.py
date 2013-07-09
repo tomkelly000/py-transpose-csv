03e983d829601ba80410ecde2aff04eea7ca0ef8
Tom Kelly <tomkelly000@gmail.com>
2013-07-09 11:16:03 -0400

    fixed last row bug

--- transpose.py
+++ transpose.py
@@ -20,16 +20,16 @@ i = 0 # row index
 for row in array:
     j = 0 # column index
     for cell in row:
-        transpose[j][i] = cell
+        transpose[j][i] = cell.strip()
         j += 1
     i += 1
+print transpose[len(array[0]) - 1]
 
 name = filename.rpartition('.')
 
 # write to file
 transposefile = open(name[0] + '_transposed.' + name[2], 'w')
 for row in transpose:
-	transposefile.write(','.join(row))
-	transposefile.write('\n')
+	transposefile.write(','.join(row) + '\n')
 
 transposefile.close()
